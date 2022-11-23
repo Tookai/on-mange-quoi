ba4db5da13c065754a628929edde97c4205846de
Tookai <tybo.orcel@gmail.com>
2022-11-23 19:26:35 +0100

    test husky

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,13 +2,14 @@ module.exports = {
 	// Type check TypeScript files
 	'*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
+	// Type check TypeScript files
 	// Lint then format TypeScript files
 	'*.(ts|tsx)': (filenames) => [
+		'yarn tsc --noEmit',
 		`yarn eslint --fix ${filenames.join(' ')}`,
 		`yarn prettier --write ${filenames.join(' ')}`,
 	],
 
 	// Format MarkDown and JSON
-	'*.(md|json)': (filenames) =>
-		`yarn prettier --write ${filenames.join(' ')}`,
+	'*.(md|json)': (filenames) => `yarn prettier --write ${filenames.join(' ')}`,
 }
